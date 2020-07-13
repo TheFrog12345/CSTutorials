9cf5dad764ddacdec57398af74f91868afca05b1
Fred Liu <fredliu876@gmail.com>
2020-07-13 15:57:28 -0400

    Added personal paragraphs for contributors

--- client/src/components/PersonalProfile.vue
+++ client/src/components/PersonalProfile.vue
@@ -1,56 +1,67 @@
 <template>
-    <v-row
-        class="card-replicate"
-    >
-        <v-col
-            class="container-col"
-            lg="1"
-            cols="2"
-        >
-            <v-img
-                height="100%"
-                width="100%"
-                :src='require("../assets/" + photo)'
-            />
-        </v-col>
-        <v-col
-            class="container-col"
-            lg="9"
-            cols="8"
-        >
-            <span class="text-h6">
-                {{ name }}
-            </span>
-            <span class="text-subtitle-1">
-                {{ role }}
-            </span>
-        </v-col>
-        <v-col
-            class="icon-col container-col"
-            cols="2"
-        >
-            <div class="icon-container">
-                <v-tooltip
-                    bottom
-                    v-for="(contact, index) in contactIcons"
+    <v-container class="card-replicate">
+        <v-row class="information-row">
+            <v-col
+                class="container-col"
+                lg="1"
+                cols="2"
+            >
+                <v-img
+                    height="100%"
+                    width="100%"
+                    :src='require("../assets/" + photo)'
+                />
+            </v-col>
+            <v-col
+                class="container-col"
+                lg="9"
+                cols="8"
+            >
+                <span class="text-h6">
+                    {{ name }}
+                </span>
+                <span class="text-subtitle-1">
+                    {{ role }}
+                </span>
+            </v-col>
+            <v-col
+                class="icon-col container-col"
+                cols="2"
+            >
+                <div class="icon-container">
+                    <v-tooltip
+                        bottom
+                        v-for="(contact, index) in contactIcons"
+                        :key="index"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                                icon
+                                v-on="on"
+                                v-bind="attrs"
+                                :href="contact.link"
+                                target="_blank"
+                            >
+                                <v-icon size="24px"> {{ contact.icon }} </v-icon>
+                            </v-btn>
+                        </template>
+                        <span> {{ contact.info }} </span>
+                    </v-tooltip>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="10">
+                <p
+                    class="px-4"
+                    v-for="(paragraph, index) in description"
                     :key="index"
                 >
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            icon
-                            v-on="on"
-                            v-bind="attrs"
-                            :href="contact.link"
-                            target="_blank"
-                        >
-                            <v-icon size="24px"> {{ contact.icon }} </v-icon>
-                        </v-btn>
-                    </template>
-                    <span> {{ contact.info }} </span>
-                </v-tooltip>
-            </div>
-        </v-col>
-    </v-row>    
+                    {{ paragraph }}
+                </p>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -68,6 +79,10 @@
             role: {
                 type: String,
                 required: true
+            },
+            description: {
+                type: Array,
+                required: false
             },
             contactIcons: {
                 type: Array,
@@ -90,12 +105,13 @@
 </script>
 
 <style scoped>
-
-    .row {
+    .container {
+        padding: 0 16px
+    }
+    .information-row {
         /*background-color: #00C853;*/
         height: 125px;
     }
-
     .container-col {
         height: 100%;
     }
