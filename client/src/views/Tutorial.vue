0ebae1e4c37b9a820dd631048e6771218ab893c1
Fred Liu <fredliu876@gmail.com>
2020-07-13 16:06:25 -0400

    Created tutorial page layout

---
+++ client/src/views/Tutorial.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="mt-14">
+        <VueScrollspy
+            :sections="sections"
+        />
+        <v-container>
+            <v-row>
+                <v-col>
+                    <div>
+                        <span class="text-h4 mt-4 section-header"> {{ title }} <span class="section-anchor" id="header1"></span> </span>
+                        <span class="text-subtitle-2"> {{ subtitle }} </span>
+                    </div>
+                    <div
+                        v-for="(part, index) in tutorialParts"
+                        :key="index"
+                    >
+                        <span
+                            v-if="part.group=='header'"
+                            class="mb-4 text-h6 mt-8 section-header"
+                        > {{ part.text }} <span class="section-anchor" :id="'header'+(part.number+1)"></span> </span>
+                        <span
+                            v-if="part.group=='paragraph'"
+                            class="mb-4 text-body-2"
+                        > {{ part.text }} </span>
+                        <span
+                            v-if="part.group=='command'"
+                            class="ml-8 mb-4 text-body-2 command"
+                        > {{ part.text }} </span>
+                        <v-img
+                            v-if="part.group=='image'"
+                            :src='require("../assets/" + part.text)'
+                        >
+                        </v-img>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    import VueScrollspy from "@/components/VueScrollspy.vue"
+    export default {
+        name: "Tutorial",
+        components: {
+            VueScrollspy
+        },
+        data: () => {
+            return {
+                sections: []
+            }
+        },
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            subtitle: {
+                type: String,
+                required: false
+            },
+            tutorialParts: {
+                type: Array,
+                required: true,
+                group: {
+                    type: String,
+                    required: true
+                },
+                text: {
+                    type: String,
+                    required: true
+                },
+                number: {
+                    type: Number,
+                    required: false
+                }
+            }
+        },
+        methods: {
+            
+        },
+        mounted() {
+            this.sections.push({name: this.title, active: false})
+            for (let i = 0; i < this.tutorialParts.length; i++) {
+                if (this.tutorialParts[i].group == 'header') {
+                    this.sections.push({name: this.tutorialParts[i].text, active: false})
+                }
+            }
+            let targets = document.querySelectorAll('.section-header')
+            let previousDistance = 10000
+            for (let i = 0; i < targets.length; i++) {
+                let offsetTop = targets[i].getBoundingClientRect().top-56
+                if (offsetTop >= 0) {
+                    for (let j = 0; j < this.sections.length; j++) {
+                        this.sections[j].active = false
+                    }
+                    
+                    
+                    offsetTop < previousDistance ? this.sections[i].active = true:this.sections[i-1].active = true
+                    break
+                }
+                previousDistance = offsetTop*-1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    span {
+        display: block;
+    }
+    .container div {
+        margin-left: 8px
+    }
+    .section-anchor {
+        display: inline;
+        position: relative;
+        top: -64px;
+        visibility: hidden;
+    }
+    .command {
+        font-weight: bold;
+        color: black;
+    }
+</style>
