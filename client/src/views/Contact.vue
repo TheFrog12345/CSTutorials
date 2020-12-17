49d09947d1ff625cb966cb148ff3cd89a483662b
Fred Liu <fredliu876@gmail.com>
2020-12-17 11:33:07 -0500

    Updated contact information

--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="mt-14">
-        <v-container>
+        <div v-if="loadedParameter!='contact'" class="text-center">
+            <span class="text-overline mt-16" style="display: block;">Error 404: Page not found </span>
+        </div>
+        <v-container v-else>
             <v-row class="text-center">
                 <v-col>
                     <v-list>
@@ -23,8 +26,6 @@
                 :contactIcons="contributor.contactIcons"
                 class="mt-8"
             />
-            <v-divider class="mt-8 mb-8"/>
-            <span class="text-h6" v-html="'We would like to express a special thank you to <a target=_blank href=https://www.linkedin.com/in/frank-liu-2b82b5179/>Frank Liu</a> for having taught both of us data structures!'"></span>
         </v-container>
     </div>
 </template>
@@ -38,6 +39,7 @@
         },
         data: () => {
             return {
+                loadedParameter: '',
                 contributors: [
                     {
                         photo: "fred.jpg",
@@ -45,12 +47,12 @@
                         role: 'Frontend and backend developer. Tutorial writer.',
                         description: [
                             `
-                                Hello! My name is Fred Liu and I'm going into my first year at the University of Waterloo for a Bachelor of Computer Science.
+                                Hello! My name is Fred Liu and I'm in my first year of Computer Science at the University of Waterloo.
                                 I'm very passionate about computer science, knowing that I can make cool, entertaining or beneficial apps from just my laptop.
                                 I hope to make a positive impact using my skills on technology!
                             `,
                             `
-                                You can also find my personal website at <a href=https://fred-online-site.netlify.app>fred-online-site.netlify.app</a>.
+                                You can learn more about me on my personal website at <a href=https://fred-online-site.netlify.app target=_blank>fred-online-site.netlify.app</a>.
                             `
                         ],
                         contactIcons: [
@@ -76,7 +78,7 @@
                         name: 'Yang Lu',
                         role: 'Backend developer and tutorial writer.',
                         description: [
-                            `Hi there! My name is Yang Lu and I’ll be starting my first year of Computer Engineering at the University of Waterloo in September. I’ve always been passionate about building things and problem-solving and seeing that computer science can be used as a tool to drive my passion, it greatly motivates me to learn more about it. I will use my skills in engineering and computer science to solve problems around me and make the world a better place.`,
+                            `Hi there! My name is Yang Lu and I’m in my first year of Computer Engineering at the University of Waterloo in September. I’ve always been passionate about building things and problem-solving and seeing that computer science can be used as a tool to drive my passion, it greatly motivates me to learn more about it. I will use my skills in engineering and computer science to solve problems around me and make the world a better place.`,
                             `In my free time, you can find me playing chess online, relaxing with some video games, or messing around on the piano, among other things. Contact me anytime, I’ll be happy to hear from you!`
                         ],
                         contactIcons: [
@@ -99,6 +101,17 @@
                     }
                 ]
             }
+        },
+        watch: {
+            '$route' (to) {
+                let newPage = to.params.page.toLowerCase()
+                if (newPage != this.loadedParameter) {
+                    this.loadedParameter = newPage
+                }
+            }
+        },
+        mounted() {
+            this.loadedParameter = this.$route.params.page.toLowerCase()
         }
     }
 </script>
